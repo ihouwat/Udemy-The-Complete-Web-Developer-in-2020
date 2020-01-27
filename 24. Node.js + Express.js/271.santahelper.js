fabd5302d143db785d75f817d47cf0e00d3b5fc7
Igor Houwat <58915539+ihouwat@users.noreply.github.com>
2020-01-27 17:32:24 -0500

    Update 271.santahelper.js

--- 24. Node.js + Express.js/271.santahelper.js
+++ 24. Node.js + Express.js/271.santahelper.js
@@ -15,9 +15,9 @@ const floorNumber = () => {
   }
   return 0
 }
-
-console.log(floorNumber())
 console.timeEnd('first')
+console.log(floorNumber())
+
 
 
 // Looping method for next approaches
@@ -29,32 +29,80 @@ const arrayLoop = () => {
   }
 }
 
-// SECOND METHOD: 1.065ms
+// SECOND METHOD, fastest time: 1.065ms
 console.time('second')
 const dataArray = fs.readFileSync('./santahelperinput', 'utf-8').split('');
 const floorNumber2 = () => {
-  num = 1;
+  num = 0;
   for(i=0; i <= dataArray.length; i++) {
     arrayLoop()
   }
+  console.timeEnd('second');
   console.log(num)
 }
 
 floorNumber2();
-console.timeEnd('second');
 
 //2. When does Santa first enter the basement
-// FINDING the basement: 0.525ms
-console.time('position');
+// FINDING the basement, fastest time: 0.104ms
 const findBasement = () => {
+  console.time('position');
   num = 1;
     for (i=0; i <= dataArray.length; i++) {
       if (num > 0) {
         arrayLoop()
       } else {
+        console.timeEnd('position');
         return i;
       }
   }
 }
 console.log(findBasement());
-console.timeEnd('position');
+
+
+
+
+
+// Andrei's solution to question 1, fastest time: 3.110ms
+function question1() {
+  console.time('Andrei1');
+  fs.readFile('./santahelperinput', (err, data) => {
+    const directions = data.toString(); // turns data to string
+    const directionsArray = directions.split('');
+    const answer = directionsArray.reduce((acc, currentValue) => {
+      if (currentValue === '(') {
+        return acc + 1
+      } else if (currentValue === ')') {
+        return acc -= 1
+      }
+    }, 0)
+    console.timeEnd('Andrei1');
+    console.log('floor:', answer);
+  })
+}
+
+question1()
+
+// Andrei's solution to question 2, fastest time: 2.098ms
+function question2() {
+  console.time('Andrei2');
+  fs.readFile('./santahelperinput', (err, data) => {
+    const directions = data.toString(); // turns data to string
+    const directionsArray = directions.split('');
+    let accumulator = 0;
+    let counter = 0;
+    const answer = directionsArray.some((currentItem) => {
+      if (currentItem === '(') {
+        accumulator + 1
+      } else if (currentItem === ')') {
+        accumulator -= 1
+      }
+      counter ++; // increases counter everytime we go through a loop
+      return accumulator < 0; // stop once it is less than 0, ending the function
+    })
+    console.timeEnd('Andrei2');
+    console.log('basement entered at:', counter); 
+  })
+}
+
+question2()
